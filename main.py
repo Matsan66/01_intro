0da8b5edd16fb0b978faad34d5b4e151213f6cad
Mats Sandén <mats.sanden@student.nbi-handelsakademin.se>
2026-05-11 17:16:04 +0200

    First project commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import math
+
+first_name = "Mats"
+last_name = "Sandén"
+
+print("My first name is " + first_name + " and last name is " + last_name)
+
+print(f"My first name is {first_name} och and last name is {last_name}")
+
+print(math.pow(10, 2))
+
